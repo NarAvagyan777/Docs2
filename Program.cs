0aba98b919ce91700f5f67f4e60412d942dd894e
NarAvagyan777 <narekavagyan003@icloud.com>
2025-02-22 15:46:14 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System ;
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Enter your name");
+        string name1 = Console.ReadLine(); 
+        Console.WriteLine("enter your favorite language");
+        string name2 = Console.ReadLine();
+        int num = DateTime.Now.Year;
+        Console.WriteLine("your name is -> " + name1);
+        Console.WriteLine("your favorite language is ->" + name2);
+        Console.WriteLine("year is ->" + num);
+    }
+}
